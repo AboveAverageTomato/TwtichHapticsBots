e4d065779f2fa60a6c9e96f3fef7e3b243724e32
AboveAverageTomato <83777757+AboveAverageTomato@users.noreply.github.com>
2021-05-06 23:54:38 -0400

    Delete devCreds.py

--- devCreds.py
+++
@@ -1,5 +0,0 @@
-# channel_name = "aboveaveragetomatobot"
-# streamer_id = "668650747"  # Bot for dev testing
-# client_id = "vmumqc7q19wjyi9j5a45qzlcx6hkj0"
-# client_secret = "c69y7c8w3dil93851yqf32fv354zws"
-# irc_token = "oauth:rg2onkzv12lvwazj3essb8103y5cpi"
